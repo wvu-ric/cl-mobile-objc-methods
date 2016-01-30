1fc9144f8fb3b3deb0f44d640955024ca0126a1e
rickykirkendall <rickykirkendall@flocoapps.com>
2016-01-30 16:16:56 -0500

    initial commit

---
+++ Methods/Methods/main.m
@@ -0,0 +1,42 @@
+//
+//  main.m
+//  Methods
+//
+//  Created by Ricky Kirkendall on 1/30/16.
+//  Copyright © 2016 CodeLab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        NSString *myString = @"I feel like we've greeted the world enough already so we'll just stick with a casual nod at this point and move past the formal greetings.";
+        
+        NSString *shoutyString = @""; // Set shoutyString to the upper case version of myString.
+        
+        NSInteger stringLength = 0; // Set stringLength to the length of myString
+        
+        NSString *sNum = @"43";
+        
+        NSInteger num = 0; // Set num to the integer value of sNum
+        
+        // This is a common initializer for NSDate
+        NSDate *longLongTimeAgo = [NSDate distantPast];
+        
+        // Every NSObject has a method called "description"
+        // Use your NSLog statement to get the description of longLongTimeAgo
+        
+        /*
+         
+         Paste the description of longLongTimeAgo here:
+         
+         
+         
+         */
+        
+        
+        
+    }
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# cl-mobile-objc-methods
-Objective-C Methods
+# Objective-C Methods
+
+As the name suggests, Objective-C uses a ton of objects. Most of these objects have methods that do all sorts of things. `NSString` and `NSDate` are both objects that come with the Foundation framework. 
+
+We'll be making heavy use of Foundation objects once we start developing iOS apps so lets go ahead and get acquainted with finding and using methods on these objects. Make sure check the class references when trying searching for methods.
