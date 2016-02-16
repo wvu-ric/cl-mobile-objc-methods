02df5daf1aaa98ae2bb6a01b315b11b91de0798c
rwatalin <rwatalin@mix.wvu.edu>
2016-02-16 16:56:49 -0500

    Completed method assignment
    Logan looked at it and approved :P
    He also said, enjoy CHINA :D

--- Methods/Methods/main.m
+++ Methods/Methods/main.m
@@ -12,25 +12,38 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
         NSString *myString = @"I feel like we've greeted the world enough already so we'll just stick with a casual nod at this point and move past the formal greetings.";
+        NSLog(@"%@", myString);
         
-        NSString *shoutyString = @""; // Set shoutyString to the upper case version of myString.
+        // Set shoutyString to the upper case version of myString.
+        NSString *shoutyString = [myString uppercaseString];
+        NSLog(@"%@", shoutyString);
         
-        NSInteger stringLength = 0; // Set stringLength to the length of myString
+        // Set stringLength to the length of myString
+        NSInteger stringLength = [myString length];
+        long stringLengthLong = (long) stringLength;
+        NSLog(@"%li", stringLengthLong);
         
         NSString *sNum = @"43";
+        NSLog(@"%@", sNum);
         
-        NSInteger num = 0; // Set num to the integer value of sNum
+        // Set num to the integer value of sNum
+        NSInteger num = [sNum intValue];
+        long numLong = (long) num;
+        NSLog(@"%li", numLong);
         
         // This is a common initializer for NSDate
         NSDate *longLongTimeAgo = [NSDate distantPast];
+        NSLog(@"%@", longLongTimeAgo);
         
         // Every NSObject has a method called "description"
         // Use your NSLog statement to get the description of longLongTimeAgo
+        NSString * timeAgoString = [longLongTimeAgo description];
+        NSLog(@"%@", timeAgoString);
         
         /*
          
-         Paste the description of longLongTimeAgo here:
-         
+        Paste the description of longLongTimeAgo here:
+        0000-12-30 00:00:00 +0000
          
          
          */
